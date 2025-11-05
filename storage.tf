20e0cb6ffc3658472a897050f7201bcf5a5d64e3
Masood Ahmed <me@ahmedmasood.com>
2025-11-04 23:35:37 -0500

    Deleting the not needed bucket oci-restic-backup

--- storage.tf
+++ storage.tf
@@ -1,9 +1,3 @@
-resource "oci_objectstorage_bucket" "oci-restic-postgresql_bucket" {
-  compartment_id = var.oci_compartment_ocid
-  name           = "oci-restic-backup"
-  namespace      = var.oci_object_storage_namespace
-}
-
 resource "oci_objectstorage_bucket" "oci-restic-auth-server" {
   compartment_id = var.oci_compartment_ocid
   name           = "oci-restic-auth-server"
