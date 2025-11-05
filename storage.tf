ce19183ee50631514d6b5bb7c5a9f745dc4e6388
Masood Ahmed <me@ahmedmasood.com>
2025-11-04 23:26:42 -0500

    Creating buckets
    - oci-restic-auth-server
    - oci-restic-vpn-server

--- storage.tf
+++ storage.tf
@@ -3,3 +3,15 @@ resource "oci_objectstorage_bucket" "oci-restic-postgresql_bucket" {
   name           = "oci-restic-backup"
   namespace      = var.oci_object_storage_namespace
 }
+
+resource "oci_objectstorage_bucket" "oci-restic-auth-server" {
+  compartment_id = var.oci_compartment_ocid
+  name           = "oci-restic-auth-server"
+  namespace      = var.oci_object_storage_namespace
+}
+
+resource "oci_objectstorage_bucket" "oci-restic-vpn-server" {
+  compartment_id = var.oci_compartment_ocid
+  name           = "oci-restic-vpn-server"
+  namespace      = var.oci_object_storage_namespace
+}
