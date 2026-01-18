c022ffc8842c2a97d9ec88de008c1cb7ac174633
Masood Ahmed <me@ahmedmasood.com>
2026-01-18 09:36:46 -0500

    fix(storage): Remove unused bucket configuration for auth server
    Eliminate redundant resource definition for
    oci-restic-auth-server bucket to streamline the
    Terraform configuration.

--- storage.tf
+++ storage.tf
@@ -1,9 +1,3 @@
-resource "oci_objectstorage_bucket" "oci-restic-auth-server" {
-  compartment_id = var.oci_compartment_ocid
-  name           = "oci-restic-auth-server"
-  namespace      = var.oci_object_storage_namespace
-}
-
 resource "oci_objectstorage_bucket" "oci-restic-vpn-server" {
   compartment_id = var.oci_compartment_ocid
   name           = "oci-restic-vpn-server"
